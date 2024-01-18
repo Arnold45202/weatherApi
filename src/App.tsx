bd3ad1f147cb2739ae5f22f6ce4a8738b46eef5f
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-18 23:10:40 +1300

    adding the weather into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Weather from './weather';
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Forecasting API</h1>
-
+        <Weather />
     </div>
   );
 }
